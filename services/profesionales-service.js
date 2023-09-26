c51349cea9e7a7f7eef7a0bf747019435f334980
Marcs202 <marcosjhs557@gmail.com>
2023-09-26 16:17:25 -0600

    listo para presentar una parte

--- services/profesionales-service.js
+++ services/profesionales-service.js
@@ -1,49 +1,51 @@
 const oracledb = require('oracledb');
-module.exports = class ProfesionalesService{
-    constructor(){}
+module.exports = class ProfesionalesService {
+    constructor() { }
     static async init() {
-        console.log(`process.env.DB_USER:admin`);//admin
-        console.log(`process.env.DB_PASSWORD:EstaPassEsLarga123`);//EstaPassEsLarga123
-        console.log(`process.env.CONNECT_STRING:skillsdb_high`);//copiala de la nube
+        console.log(`process.env.DB_USER: ${process.env.DB_USER}`);//admin
+        console.log(`process.env.DB_PASSWORD:${process.env.DB_PASSWORD}`);//EstaPassEsLarga123
+        console.log(`process.env.CONNECT_STRING:${process.env.CONNECT_STRING}`);//skillsdb_high
+        console.log(`process.env.DIRECCION:${process.env.DIRECCION}`)
         console.log('Creando pool de conexiones...');
-
-
-        oracledb.initOracleClient({ libDir: 'C:\\oracle\\instantclient_21_11' });
+        //'C:\\oracle\\instantclient_21_11' ;
+        const direccion =  process.env.DIRECCION;
+        const user= process.env.DB_USER;
+        const password = process.env.PASSWORD;
+        const connectString = process.env.CONNECT_STRING;
+        oracledb.initOracleClient({ libDir: direccion });
 
         await oracledb.createPool({
-            user: "admin",
-            password: "EstaPassEsLarga123",
-            connectString: "skillsdb_high",
+            user: user,
+            password: password,
+            connectString: connectString,
         });
-        
+
         console.log('Pool de conexiones creado.')
         return new ProfesionalesService();
     }
-    async getAllTop(){
+    async getAllTop() {
         let connection;
-        const profesionales=[];
+        const profesionales = [];
         try {
             connection = await oracledb.getConnection()
             let sql = `SELECT ID, NOMBRE, FOTO_PERFIL FROM (SELECT perfiles_profesionales.*, ROWNUM AS ranking
                     FROM perfiles_profesionales ORDER BY calificacion_promedio DESC)WHERE ranking <= 10 `;
             //let result = await connection.execute(sql,binds,{autoCommit});
-            let autoCommit=false;
-            let result = await connection.execute(sql,[],{autoCommit});
-        result.rows.map(profesional=>{
-            let schemaProfesional ={
-                "ID":profesional[0],
-                "NOMBRE":profesional[1],
-                "FOTOGRAFIA":profesional[2]
-            }
-            profesionales.push(schemaProfesional);
-            
-        });
-//        console.log(profesionales)
-        
+            let autoCommit = false;
+            let result = await connection.execute(sql, [], { autoCommit });
+            result.rows.map(profesional => {
+                let schemaProfesional = {
+                    "ID": profesional[0],
+                    "NOMBRE": profesional[1],
+                    "FOTOGRAFIA": profesional[2]
+                }
+                profesionales.push(schemaProfesional);
+
+            });
         } catch (error) {
             console.error(error);
         }
-        finally{
+        finally {
             try {
                 await connection.close();
             }
