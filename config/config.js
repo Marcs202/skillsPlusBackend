2e0dea664c8c01b4f5fdcff9f99fd879c5cd4a56
Marcs202 <marcosjhs557@gmail.com>
2023-10-24 23:42:42 -0600

    probando express-upload, modifique el config.js

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
-const direccion ='C:\\oracle\\instantclient_21_11'; //process.env.ORACLE_LIB_DIR;
-const user= 'admin'; //process.env.DB_USER;
-const password = 'EstaPassEsLarga123'; //process.env.DB_PASSWORD;
-const connectString = 'skillsdb_high'//process.env.CONNECT_STRING ;
+const direccion =process.env.ORACLE_LIB_DIR;
+const user= process.env.DB_USER;
+const password =process.env.DB_PASSWORD;
+const connectString = process.env.CONNECT_STRING ;
 
 module.exports={
   direccion,user,password,connectString
